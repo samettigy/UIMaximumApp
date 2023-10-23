90f2c5ec8a94e70b0f45bb403be19acd245b1167
samettigy <116752451+samettigy@users.noreply.github.com>
2023-10-23 09:10:47 +0300

    Initial Commit

---
+++ UIMaximumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIMaximumApp
+//
+//  Created by Samet TIG on 23.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
