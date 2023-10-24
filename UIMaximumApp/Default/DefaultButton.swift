1470718ab83970529ddf932711d5b21c3a2421c2
samettigy <116752451+samettigy@users.noreply.github.com>
2023-10-24 11:49:34 +0300

    Added Up Menu and Scrollview

---
+++ UIMaximumApp/Default/DefaultButton.swift
@@ -0,0 +1,20 @@
+//
+//  DefaultButton.swift
+//  UIMaximumApp
+//
+//  Created by Samet TIG on 23.10.2023.
+//
+
+import UIKit
+
+class DefaultButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
